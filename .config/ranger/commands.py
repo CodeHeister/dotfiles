47069dbdf0f542719f06967c146a3b5044dd7629
CodeHeister <extremal.user@gmail.com>
2022-01-30 00:24:02 +0200

    SVG support, fzf search function and image preview with ueberzug

---
+++ .config/ranger/commands.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+import ranger.api.commands as ranger
+
+
+class fzf(ranger.Command):
+    """
+    :fzf_select
+    Find a file using fzf.
+    With a prefix argument select only directories.
+    """
+
+    def execute(self):
+        if self.quantifier:
+            # match only directories
+            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        else:
+            # match files and directories
+            command = "find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, _ = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
