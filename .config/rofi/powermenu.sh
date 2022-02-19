cd66db1d864ce3e432c79378c5d5475771064788
CodeHeister <extremal.user@gmail.com>
2022-02-19 11:50:11 +0200

    Rofi fix

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -5,7 +5,8 @@ if [ -z "$@" ]; then
     echo -en "Shutdown\0icon\x1fsystem-shutdown\n"
     echo -en "Logout\0icon\x1fsystem-log-out\n"
     echo -en "Suspend\0icon\x1fsystem-suspend\n"
-    echo -en "Reboot\0icon\x1fsystem-reboot\n"
+	echo -en "Reboot\0icon\x1fsystem-reboot\n"
+	echo -en "Lock\0icon\x1fsystem-lock-screen\n"
 else
     if [ "$1" = "Shutdown" ]; then
         systemctl poweroff
@@ -15,5 +16,7 @@ else
         systemctl reboot
     elif [ "$1" = "Suspend" ]; then
         systemctl suspend
+	elif [ "$1" = "Lock" ]; then
+		loginctl lock-session
     fi
 fi
